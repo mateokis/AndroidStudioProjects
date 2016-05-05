3e57ca2671e23fb8192a401b84a9ae9a4740cb88
Mateo Kis <mateo.kis@skole.hr>
2016-05-05 12:55:35 +0200

    projekti

--- Aktivnosti/app/src/main/java/com/example/android/aktivnosti/Aktivnosti.java
+++ Aktivnosti/app/src/main/java/com/example/android/aktivnosti/Aktivnosti.java
@@ -1,7 +1,11 @@
 package com.example.android.aktivnosti;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class Aktivnosti extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class Aktivnosti extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+}
+    @Override public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.prva_aktivnost) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    public void OtvoriPrvuAktivnost(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
